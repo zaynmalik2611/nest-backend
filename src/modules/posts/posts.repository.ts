6d6072e332f047890dd26a2f0c8de83dd09962f4
zaynmalik2611 <zabideen.bscs17seecs@seecs.edu.pk>
2023-02-14 18:51:25 +0500

    feat: add post module and repository layer

---
+++ src/modules/posts/posts.repository.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Post, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+type GetPostsQueryType = {
+  skip?: number;
+  take?: number;
+  cursor?: Prisma.PostWhereUniqueInput;
+  where?: Prisma.PostWhereInput;
+  orderBy?: Prisma.PostOrderByWithRelationInput;
+};
+@Injectable()
+export class PostsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async createPost(params: { data: Prisma.PostCreateInput }): Promise<Post> {
+    const { data } = params;
+    return this.prisma.post.create({ data });
+  }
+
+  async getPosts(params: GetPostsQueryType): Promise<Post[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.post.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async updatePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+    data: Prisma.PostUpdateInput;
+  }): Promise<Post> {
+    const { where, data } = params;
+    return this.prisma.post.update({ where, data });
+  }
+
+  async deletePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+  }): Promise<Post> {
+    const { where } = params;
+    return this.prisma.post.delete({ where });
+  }
+}
