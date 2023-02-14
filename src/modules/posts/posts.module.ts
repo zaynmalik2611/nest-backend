cc628609545f5e5b72cb1a982b7f85bf66b25a17
zaynmalik2611 <zabideen.bscs17seecs@seecs.edu.pk>
2023-02-14 19:31:32 +0500

    feat: add service layer for creating and retrieving posts

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PostsRepository } from './posts.repository';
+import { PostsService } from './posts.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [PostsRepository],
+  providers: [PostsRepository, PostsService],
+  exports: [PostsService],
 })
 export class PostsModule {}
